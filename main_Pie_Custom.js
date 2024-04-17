4581e41ef58299fa8f86075cfdf4c1a9630bcc04
donikaaaaas <148459556+donikaaaaas@users.noreply.github.com>
2024-04-17 14:27:49 +0200

    Update main_Pie_Custom.js

--- main_Pie_Custom.js
+++ main_Pie_Custom.js
@@ -1,29 +1,21 @@
-/*var getScriptPromisify = (src) => {
-  return new Promise((resolve) => {
-    $.getScript(src, resolve);
-  });
-};*/
-
 var getScriptPromisify = (src) => {
-  // Workaround with conflict between geo widget and echarts.
-  const __define = define
-  define = undefined
-  return new Promise(resolve => {
+  const __define = define;
+  define = undefined;
+  return new Promise((resolve) => {
     $.getScript(src, () => {
-      define = __define
-      resolve()
-    })
-  })
-}
+      define = __define;
+      resolve();
+    });
+  });
+};
 
 (function () {
   const prepared = document.createElement("template");
   prepared.innerHTML = `
-        <style>
-        </style>
-        <div id="root" style="width: 100%; height: 100%;">
-        </div>
-      `;
+    <style>
+    </style>
+    <div id="root" style="width: 100%; height: 100%;"></div>
+  `;
   class CustomPieSample extends HTMLElement {
     constructor() {
       super();
@@ -50,7 +42,6 @@ var getScriptPromisify = (src) => {
     async render() {
       await getScriptPromisify(
         "https://cdn.staticfile.org/echarts/5.3.0/echarts.min.js"
-        
       );
 
       if (!this._myDataSource || this._myDataSource.state !== "success") {
@@ -62,71 +53,63 @@ var getScriptPromisify = (src) => {
       const data = this._myDataSource.data.map((data) => {
         return {
           name: data[dimension].label,
-          value: data[measure].raw
-        }
-      }).sort(function(a, b){
-        return a.value - b.value
-      })
+          value: data[measure].raw,
+        };
+      }).sort((a, b) => a.value - b.value);
 
-      const myChart = echarts.init(this._root, "wight")
+      const myChart = echarts.init(this._root, "wight");
       const option = {
-        backgroundColor: '',
+        backgroundColor: "",
         title: {
-          text: '',
-          left: 'center',
+          text: "",
+          left: "center",
           top: 20,
           textStyle: {
-            color: ''
-          }
+            color: "",
+          },
         },
         tooltip: {
-          trigger: 'item'
+          trigger: "item",
         },
         visualMap: {
           show: false,
           min: 0,
           max: data[data.length - 1].value * 1.5,
           inRange: {
-            colorLightness: [0, 1]
-          }
+            colorLightness: [0, 1],
+          },
         },
         series: [
           {
-            name: '',
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '50%'],
+            name: "",
+            type: "pie",
+            radius: ["50%", "70%"], // Inner and outer radius for the donut chart
+            center: ["50%", "50%"],
             data,
-         
-            roseType: 'radius',
             label: {
-              color: '#1D2D3E'
+              formatter: "{b}: {d}%", // Display label as name: percentage
             },
             labelLine: {
-              lineStyle: {
-                color: '#1D2D3E'
-              },
-              smooth: 0.2,
               length: 10,
-              length2: 20
+              length2: 20,
             },
             itemStyle: {
-              color: '#0070F2',
+              color: "#0070F2",
               shadowBlur: 15,
-              shadowColor: 'rgba(0, 0, 0, 0.3)'
+              shadowColor: "rgba(0, 0, 0, 0.3)",
             },
-            animationType: 'scale',
-            animationEasing: 'elasticOut',
-            animationDelay: function (idx) {
-              return Math.random() * 200;
-            }
-            
-          }
-        ]
+            animationType: "scale",
+            animationEasing: "elasticOut",
+            animationDelay: (idx) => Math.random() * 200,
+          },
+        ],
       };
       myChart.setOption(option);
     }
   }
 
-  customElements.define("com-sap-sample-echarts-custom_pie_chart", CustomPieSample);
+  customElements.define(
+    "com-sap-sample-echarts-custom_pie_chart",
+    CustomPieSample
+  );
 })();
