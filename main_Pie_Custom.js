a8a2a01e20ae20871788225aa37d431be0c9a994
donikaaaaas <148459556+donikaaaaas@users.noreply.github.com>
2024-04-19 08:42:23 +0200

    Create main_Pie_Custom.js

---
+++ main_Pie_Custom.js
@@ -0,0 +1,32 @@
+option = {
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '5%',
+    left: 'center'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 40,
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: 
+    }
+  ]
+};
